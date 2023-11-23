13bcc5493b6f3a4aed68f017f8a369488188f2f5
Jagroop Singh <sjagroop212@gmail.com>
2023-11-23 12:14:08 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <Box>
+      <Typography variant="h1">Hello world!</Typography>
+    </Box>
+  );
+}
+
+export default App;
